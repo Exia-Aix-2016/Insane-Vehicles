927c2cba6007dcd9c90dabfa81716252719558c7
Hougo13 <hougo13@gmail.com>
2017-05-17 15:57:25 +0200

    create MyVehicle class

---
+++ src/fr/exia/insanevehicles/first/element/MyVehicle.java
@@ -0,0 +1,12 @@
+package fr.exia.insanevehicles.first.element;
+
+public class MyVehicle extends Element {
+
+    private int x, y;
+    private static final char SPRITE = 'H';
+
+    MyVehicle(){
+        super(SPRITE);
+    }
+    
+}
