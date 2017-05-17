ed10f66ca0d90c184c610c5cf556fd5e772f60fb
NooGat <kinghyde111@gmail.com>
2017-05-17 14:50:29 +0200

    class Element

--- src/MyVehicle.java
+++ src/MyVehicle.java
@@ -1,3 +1,23 @@
-public class MyVehicle {
+public class MyVehicle extends Element{
+    private int x;
+    private int y;
+
+
+    public void setX(int x) {
+        this.x = x;
+    }
+    public int getX() {
+        return x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    public int getY() { return y; }
+
+    public MyVehicle(){
+        
+    }
+
 
 }
