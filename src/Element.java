39112f3a846a735cc722404a583edda016acd978
mandel <mandel.vaubourg@viacesi.fr>
2017-05-17 15:04:19 +0200

    mod Element

--- src/Element.java
+++ src/Element.java
@@ -1,11 +1,25 @@
 public class Element {
     private char sprite;
+    private int x, y;
 
     Element(char sprite){
         this.sprite = sprite;
     }
 
-    public char getSprite() {
+    //Mutateur
+    public void setSprite(char SPRITE){//set Sprite Element
+        sprite = SPRITE;
+    }
+    public void setX(int coordX){//set coord x
+        x = coordX;
+    }
+    public void setY(int coordY){//set coord y
+        y = coordY;
+    }
+    //Assesseur
+    public char getSprite() {//Return sprite Element
         return sprite;
     }
+
+
 }
