7d88aa0f9424225d1821484f5c90b78857bad4e0
Hougo13 <hougo13@gmail.com>
2017-05-17 15:24:04 +0200

    start road

--- src/Road.java
+++ src/Road.java
@@ -1,3 +1,31 @@
+import java.util.ArrayList;
+
 public class Road {
 
+    private int width;
+    private int height;
+    private int view;
+    private int quota;
+
+    private ArrayList<Element> onTheRoad;
+
+    Road(int width, int height, int view, int quota){
+        this.width = width;
+        this.height = height;
+        this.view = view;
+        this.quota = quota;
+    }
+
+    private void fillOnTheRoad(){
+
+    }
+
+
+    public char getOnTheRoad(int x, int y){
+        return ' ';
+    }
+
+    public void setOnTheRoad(Element element, int x, int y){
+
+    }
 }
