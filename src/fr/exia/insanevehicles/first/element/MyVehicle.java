3d3ac3ae47b7a2c0a709df913e0f03dc8668ee7d
Hougo13 <hougo13@gmail.com>
2017-05-17 15:59:44 +0200

    add setters/getters

--- src/fr/exia/insanevehicles/first/element/MyVehicle.java
+++ src/fr/exia/insanevehicles/first/element/MyVehicle.java
@@ -8,5 +8,20 @@ public class MyVehicle extends Element {
     MyVehicle(){
         super(SPRITE);
     }
-    
+
+    public int getY() {
+        return y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
 }
