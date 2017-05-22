b5879a4325495b8f8353cc8723fab5c6bc82ce34
NooGat <kinghyde111@gmail.com>
2017-05-22 11:31:16 +0200

    Rename fenetre

--- src/Java/fr/exia/insanevehicles/first/Window.java
+++ src/Java/fr/exia/insanevehicles/first/Window.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 class Window extends JFrame {
     Window(){
-        this.setTitle("Ma première fenêtre Java");
+        this.setTitle("xXxInsane_VehiclexXx Christmas Edition 2017");
         this.setSize(200, 700);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
